5e0000893800f8d37f7464f16f86809f9c7466dd
olaluwa <oluwapelumiola3@gmail.com>
2022-03-15 18:49:08 +0100

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,7 @@
-#include "main.h"
-int main (void)
+#include <stdio.h>
+int main ()
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar\n");
 	
 	return 0;
 	 
