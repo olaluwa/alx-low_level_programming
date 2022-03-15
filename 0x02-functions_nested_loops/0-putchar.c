7f10b0624647a503bf370f2f95614fc75184e146
olaluwa <oluwapelumiola3@gmail.com>
2022-03-15 23:47:15 +0100

    the putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,15 @@
 #include "main.h"
 int main (void)
 {
-	char word[9] = "_putchar";
-
-	int i = 0;
-
-	while (i < 8)
-	{
-		_purchar(word[i]);
-		i++;
-
-
-
-	}
-	_putchar("\n");
-	
-	return 0;
-	 
+	_putchar('_');
+        _putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0); 
 	
 }
