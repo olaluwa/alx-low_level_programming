d073a975265ffab688ab2c359f2b608f73c938de
olaluwa <oluwapelumiola3@gmail.com>
2022-03-15 18:02:27 +0100

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+void print_alphabet_x10(void)
+{
+
+	int i;
+	char c;
+	
+	for (1 = 0; i < 10; ++1)
+	{
+	
+		for (c ='a'; c <= 'z'; c++)
+		{
+
+			_putchar(c);
+
+		}
+		_putchar('\n');
+
+	}
+
+
+
+}
