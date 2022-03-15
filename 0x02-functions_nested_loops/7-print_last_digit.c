57b5eacabd8b38c0a7b2c72fc8a8db411e980d27
olaluwa <oluwapelumiola3@gmail.com>
2022-03-16 00:40:49 +0100

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int print_last_digit(int n)
+{
+
+	int r
+		if (n < 0)
+		{
+
+			r = -1 * (n % 10);
+			_putchar(r + 0);
+			return (r);
+
+
+
+		}
+		else {
+
+			r = n % 10;
+			_putchar (r + 0);
+			return (r);
+
+		}
+
+
+
+}
