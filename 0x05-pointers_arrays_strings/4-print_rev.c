52065856fd968e3d6cd42a9c98d370d8e704d1ad
olaluwa <oluwapelumiola3@gmail.com>
2022-03-21 23:42:46 +0100

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string
+ * Return: no return
+ */
+void print_rev(char *s)
+{
+	int sam = 0;
+	while (sam >= 0)
+	{
+		if(s[sam] == '\0')
+			break;
+		count++;
+
+	}
+	for (sam--; sam >= 0; sam--)
+		_putchar(s[sam]);
+	_putcahr('\n');
+
+}
