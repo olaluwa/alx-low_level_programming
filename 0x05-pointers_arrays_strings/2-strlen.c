593257e106301c95b65b1924d2f29bfc89384ba9
olaluwa <oluwapelumiola3@gmail.com>
2022-03-21 22:13:28 +0100

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function that returns the length of a string
+ * @s: length of  a string
+ * Return: no return
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[1] != 0)
+	{
+		i++
+	}
+	return (1);
+
+}
