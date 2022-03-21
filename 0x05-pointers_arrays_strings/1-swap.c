c911b0d651eb689392b5c92e6e6ab984902bd179
olaluwa <oluwapelumiola3@gmail.com>
2022-03-21 21:50:05 +0100

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * SNIP_int - snaps two integers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: no return
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a
+	*a = *b
+	*b = *c
+}
