4be436a7e34ba18c7ded329894361394bdb2acd1
olaluwa <oluwapelumiola3@gmail.com>
2022-03-24 10:44:34 +0100

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- * print_number -print number putchar
+ * print_number - print number putchar
  * @n:integer
- * Return: void
+ * Return: no return 
  */
 void print_number(int n)
 {
