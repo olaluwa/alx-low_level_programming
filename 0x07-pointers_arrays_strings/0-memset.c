0766d6a6a02e3a54170ad23eb82c01b8991cebe5
olaluwa <oluwapelumiola3@gmail.com>
2022-03-28 16:33:43 +0100

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _memet - fill memory with a contant byte
- * @n:char
- * @s:char pointer 
- * @b:char variable 
- * Return: char
+ * @n: byte filled 
+ * @s: memory char pointer 
+ * @b: constant char variable 
+ * Return: the pointer to dest
  */
 char *_memset(char *s, char b, unsigned int n)
 {
