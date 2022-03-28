ff478f671ba1be74010e54af38f253688b5699c0
olaluwa <oluwapelumiola3@gmail.com>
2022-03-28 16:26:56 +0100

    task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,13 +6,17 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int i, j = 0;
 
-	for (i = 0; a[i][7]; i++)
+	for (i = 0; i < 64; i++)
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
+		if (i % 8 == 0 && i !=0)
+		{
+			j = i;
+			_putchar('\n');
+		}
 
-	_putchar('\n');
+		_putchar(a[i / 8][i - m]);
 	}
+	_puthar('\n');
 }
