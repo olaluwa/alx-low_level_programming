4aff99ebeb8afb65527b02b29e503b24708549c8
olaluwa <oluwapelumiola3@gmail.com>
2022-04-01 21:33:46 +0100

    task 0

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+        int a,b;
+
+        for (a = 0; dest[a], != '\0'; a++)
+        {
+                continue;
+
+        }
+
+        for (b = 0; src[b] != '\0' && b < n;  b++)
+        {
+                dest[a + b] = src[b];
+        }
+
+        dest[a + b] = '\0';
+
+        return (dest);
+}
+
