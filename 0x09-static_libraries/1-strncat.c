900621c5d3796a7086226c360c4dad492f48802c
olaluwa <99435322+olaluwa@users.noreply.github.com>
2022-04-01 21:46:56 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -6,7 +6,7 @@
  * @n: amount of bytes used from src.
  * Return: the pointer to dest.
  */
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
         int a,b;
 
